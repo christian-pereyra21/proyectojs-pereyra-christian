fd44b142841c568114c7cb63de4b52ef9e648677
Christian Pereyra <christianpereyra.ftb@gmail.com>
2023-08-30 15:40:07 -0300

    productos

--- desafio.js
+++ desafio.js
@@ -1,84 +1,35 @@
-class productos{
+class producto{
     constructor(nombre,precio){
-        this.id;
-        this.nombre;
-        this.precio;
+        this.nombre= nombre;
+        this.precio= precio;
+        this.vendido= false;
+    }
+
+    vender(){
+        this.vendido= true
     }
 }
 
-let producto1= new productos("Auriculares jbl wave",123)
-let producto2= new productos("Auriculares JBL TUNE500",123)
-let producto3= new productos("Auriculares JBL Quantum 350",)
-let producto4= new productos("Stereo Panacom CA",)
-let producto5= new productos("Stereo Pionner In Dash Avh-5350",)
-let producto6= new productos("Stereo Sony Bluethoot",)
-let producto7= new productos("Parlante Stromberg Portatil Bump",)
-let producto8= new productos("Parlante JBL Party Box 310 Hero",)
-let producto9= new productos("Parlante JBL GO 3",)
-let producto10= new productos("Controlador Pionner WE GO 4",)
-let producto11= new productos("Controlador DJ Hercules Inpulse 3",)
-let producto12= new productos("Controlador DDJ-200",)
-let carrito= []
+const productos= []
+productos.push(new producto("auriculares jbl wave",123))
+productos.push(new producto("auriculares jbl tune500",123))
+productos.push(new producto("auriculares jbl quantum 350",))
+productos.push(new producto("stereo Panacom CA",))
+productos.push(new producto("stereo Pionner In Dash Avh-5350",))
+productos.push(new producto("Stereo Sony Bluethoot",))
+productos.push(new producto("Parlante Stromberg Portatil Bump",))
+productos.push(new producto("Parlante JBL Party Box 310 Hero",))
+productos.push(new producto("Parlante JBL GO 3",))
+productos.push(new producto("Controlador Pionner WE GO 4",))
+productos.push(new producto("Controlador DJ Hercules Inpulse 3",))
+productos.push(new producto("Controlador DDJ-200",))
 
-let seleccion= prompt("Buen dia ¿Desea comprar algun producto?. Seleccione SI o NO.")
+let nombreProducto= prompt("Hola!! gracias por visitarnos, elija su producto.")
 
-while(seleccion != "si" && seleccion != "no"){
-    alert("Elija una opcion")
-let seleccion= prompt("Buen dia ¿Desea comprar algun producto?. Seleccione SI o NO.")
-}
-
-if(seleccion == "si"){
-    let todosLosProductos= productos.map(
-        (productos) => productos.nombre+ " " +productos.precio+ "$");
-    alert(todosLosProductos.join("--"))
-} else if(seleccion == "no"){
-    alert("Gracias por visitarnos!!")
-}
-
-while(seleccion != "no"){
-    let producto= prompt("Agrega un producto a tu carrito de compras")
-    let precio= 0 
-
-    if(producto == "auriculares"){
-        switch(productos){
-            case "jbl wave":
-                precio= 123
-                break;
-            case "jbl tune 500":
-                precio= 123
-                break;
-            case "jbl quantum 350":
-                precio=123
-                break;
-            
-        }
-    }
-
-    else(producto == "stereo"){
-        switch(productos){
-            case "pionner":
-                precio= 123
-                break;
-            case "panacom":
-                precio= 123
-                break;
-            case "sony":
-                precio= 123
-                break;
-        }
-    }
-
-    else(producto == "parlantes"){
-        switch(productos){
-            case "stromberg":
-                precio= 123
-                break;
-            case "jbl party box":
-                precio= 123
-                break;
-            case "jbl go 3":
-                precio= 123
-                break;
-        }
+for(let index =0; index< productos.length; index++){
+    if(productos[index].nombre === nombre){
+        alert(`tipo de producto: ${producto.nombre} precio$: ${producto.precio}`);
+    }else{
+        alert("No se encontro el producto :(")
     }
 }
